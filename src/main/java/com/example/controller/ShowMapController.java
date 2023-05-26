7fc874704f22129d91f4d06301f6f8670cfbdf92
yuta5190 <yuutatoko0000@gmail.com>
2023-05-26 11:25:20 +0900

    コメントの追加

--- src/main/java/com/example/controller/ShowMapController.java
+++ src/main/java/com/example/controller/ShowMapController.java
@@ -7,15 +7,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * map表示がめん.
+ * 
+ * @author yoshida_yuuta
+ *
+ */
 @RequestMapping("/map")
 @Controller
 public class ShowMapController {
 
+/**
+ * 仮画面表示(get時)
+ * @param model
+ * @return
+ */
 @GetMapping("/")	
-public String getIndex(Model model,Integer id) {
+public String getIndex(Model model) {
 model.addAttribute("id",1);
 return "map";}
 
+/**
+ * idを受け取る画面表示
+ * @param model
+ * @param id　駅id or 地区id
+ * @return map表示画面
+ */
 @PostMapping("/")
 public String postIndex(Model model,Integer id) {
 model.addAttribute("id",id);
