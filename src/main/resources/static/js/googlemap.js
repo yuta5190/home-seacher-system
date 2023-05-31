7ab11342d5baa4f082611a22f9239156cfbdc714
yuta5190 <yuutatoko0000@gmail.com>
2023-05-31 15:00:42 +0900

    htmlの作成

--- src/main/resources/static/js/googlemap.js
+++ src/main/resources/static/js/googlemap.js
@@ -1,17 +1,21 @@
-let latitude = 35.6811673;
-let longitude = 139.7670516;
-function initializeMap() {
+const map = new Vue({
+	el:"#map",
+mounted() {
+    this.initializeMap();
+  },
+  methods: {
+    initializeMap() {
+      let latitude = 35.6811673;
+      let longitude = 139.7670516;
 
-  var MyLatLng = new google.maps.LatLng(latitude, longitude);
-  var Options = {
-    zoom: 15,
-    center: MyLatLng,
-    mapTypeId: 'roadmap'
-  };
-  var map = new google.maps.Map(document.getElementById('map'), Options);
+      var MyLatLng = new google.maps.LatLng(latitude, longitude);
+      var Options = {
+        zoom: 15,
+        center: MyLatLng,
+        mapTypeId: 'roadmap'
+      };
+      var map = new google.maps.Map(document.getElementById('map'), Options);
+    }
+  }
 }
-
-// HTMLの読み込みが完了したらinitializeMapを実行する
-document.addEventListener('DOMContentLoaded', function() {
-  initializeMap();
-});
+);
