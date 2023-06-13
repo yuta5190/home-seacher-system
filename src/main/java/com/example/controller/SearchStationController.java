b504efe1ae2591f94514939c03718b50d5c6b7c7
YoshimatsuShouta <yryr0528@icloud.com>
2023-06-13 17:30:44 +0900

    沿線検索表示画面の初期表示を行うコントローラーとそのテストを実装しました

---
+++ src/main/java/com/example/controller/SearchStationController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.RailwayLine;
+import com.example.service.RailwayLineService;
+
+@RequestMapping("/station")
+@Controller
+public class SearchStationController {
+
+	@Autowired
+	private RailwayLineService railwayLineService;
+
+	@GetMapping("/search")
+	public String toSeatch(Model model) {
+		List<RailwayLine> railwayLineList = railwayLineService.findByWithin100000MetersTokyo();
+
+		model.addAttribute("railwayLineList", railwayLineList);
+		return "station-search.html";
+	}
+}
